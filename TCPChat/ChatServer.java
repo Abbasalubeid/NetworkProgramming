3350d108ca5de43be9bf7e62739e1733a0ab728d
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-11-01 21:53:20 +0100

    Unused port

--- TCPChat/ChatServer.java
+++ TCPChat/ChatServer.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.util.*;
 
 public class ChatServer {
-    private static final int PORT = 12345;
+    private static final int PORT = 1234;
     private static Set<ClientHandler> clientHandlers = new HashSet<>();
 
     public static void main(String[] args) {
