407d2002916bc07371e15b37e9908c13fd7725e7
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-11-05 13:40:34 +0100

    Handle different status codes

--- HTTPGuessingGame/src/view/HttpResponseBuilder.java
+++ HTTPGuessingGame/src/view/HttpResponseBuilder.java
@@ -4,15 +4,32 @@ import java.nio.charset.StandardCharsets;
 
 public class HttpResponseBuilder {
 
-    public String buildResponse(String content) {
-        String body = "<h1>" + content + "</h1>\n";
+    public String buildResponse(int statusCode, String content) {
+        String statusLine;
+        switch (statusCode) {
+            case 200:
+                statusLine = "HTTP/1.1 200 OK";
+                break;
+            case 400:
+                statusLine = "HTTP/1.1 400 Bad Request";
+                break;
+            case 404:
+                statusLine = "HTTP/1.1 404 Not Found";
+                break;
+            default:
+                statusLine = "HTTP/1.1 500 Internal Server Error";
+                break;
+        }
+
+        String body = (content != null && !content.isEmpty()) ? "<h1>" + content + "</h1>\n" : "";
         byte[] bodyBytes = body.getBytes(StandardCharsets.UTF_8);
         int contentLength = bodyBytes.length;
 
-        return "HTTP/1.1 200 OK\r\n" // HTTP response status line
+        return statusLine + "\r\n"
                 + "Content-Type: text/html; charset=UTF-8\r\n"
                 + "Content-Length: " + contentLength + "\r\n"
-                + "\r\n" // empty line to separate the headers from the body
-                + body; // HTML response body;
+                + "\r\n"
+                + body;
     }
+
 }
