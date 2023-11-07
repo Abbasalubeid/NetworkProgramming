d32d127c0d2b8438d5ead09a74246211a5ad149a
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-11-07 14:40:39 +0100

    Simulate a web browser to play the guessing game using HttpURLConnection

---
+++ HTTPGuessingGame/src/GameClient.java
@@ -0,0 +1,73 @@
+import java.net.*;
+import java.io.*;
+import java.util.Random;
+
+public class GameClient {
+
+    public static void main(String[] args) throws IOException {
+        String url = "http://localhost:8080";
+        int totalGuesses = 0;
+
+        // Play the game 100 times
+        for (int i = 1; i <= 100; i++) {
+            String cookie = null;
+            boolean gameWon = false;
+            int guesses = 0;
+
+            // Start a new game session
+            cookie = startNewGame(url);
+
+            // Keep guessing until the game is won
+            while (!gameWon) {
+                int guess = new Random().nextInt(100) + 1; // Random guess between 1 and 100
+                String response = sendGuess(url, guess, cookie);
+
+                gameWon = response.contains("Correct!"); // Check if the guess was correct
+                guesses++;
+            }
+
+            totalGuesses += guesses;
+            System.out.println("Game " + i + " won in " + guesses + " guesses");
+        }
+
+        double averageGuesses = (double) totalGuesses / 100;
+        System.out.println("Average number of guesses: " + averageGuesses);
+    }
+
+    private static String startNewGame(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        // Get cookie from header
+        String cookie = connection.getHeaderField("Set-Cookie");
+        if (cookie != null) {
+            cookie = cookie.split(";")[0]; // We only want the sessionId part of the cookie
+        }
+
+        connection.disconnect();
+        return cookie;
+    }
+
+    private static String sendGuess(String urlString, int guess, String cookie) throws IOException {
+        URL url = new URL(urlString + "?guess=" + guess);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        if (cookie != null) {
+            connection.setRequestProperty("Cookie", cookie);
+        }
+
+        // Read the response
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        connection.disconnect();
+
+        return response.toString();
+    }
+}
