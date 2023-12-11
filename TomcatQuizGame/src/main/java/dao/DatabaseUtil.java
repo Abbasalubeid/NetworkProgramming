4415a8c1391a7a29aff9969acc84c68250225843
Abbas Alubeid <abbasalubeid1@gmail.com>
2023-12-11 23:10:05 +0100

    Connect to database

---
+++ TomcatQuizGame/src/main/java/dao/DatabaseUtil.java
@@ -0,0 +1,32 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+import java.io.InputStream;
+
+public class DatabaseUtil {
+    
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            InputStream input = DatabaseUtil.class.getClassLoader().getResourceAsStream("/db.properties");
+            Properties prop = new Properties();
+            prop.load(input);
+
+            String driver = prop.getProperty("driver");
+            String url = prop.getProperty("url");
+            String user = prop.getProperty("user");
+            String password = prop.getProperty("password");
+
+            // Load the PostgreSQL JDBC driver
+            Class.forName("org.postgresql.Driver");
+            
+            // Establish the connection
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
