61d75f2e2c7da5abe3547c92a53dc81403b7c5d6
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-01-21 19:18:37 +0100

    Spring Boot application main class and servlet initializer

---
+++ SpringQuizGame/src/main/java/se/kth/SpringQuizGame/SpringQuizGameApplication.java
@@ -0,0 +1,13 @@
+package se.kth.SpringQuizGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuizGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuizGameApplication.class, args);
+	}
+
+}
